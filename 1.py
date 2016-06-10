57656b3cc63e89667977539cfe97fe48be9a76f2
ramkumarreddy <ramkumarkovvuri@gmail.com>
2016-06-10 21:28:19 +0530

    nmap to JSON for multiple IP's

--- 1.py
+++ 1.py
@@ -1,19 +1,29 @@
 import commands
-s = commands.getoutput('nmap 10.1.39.79')
-if 'PORT' in s:
-	s = s.split('\n')
-	s = filter(None,s)
-	ip = s[1].split(' ')[4]
-	arr = []
-	for i in range(5,len(s)-1):
-		data=filter(None,s[i].split(' '))
-		dic = {}
-		dic['PORT']=data[0]
-		dic['STATE']=data[1]
-		dic['SERVICE'] = data[2]
-		arr.append(dic)
-	dic1={}
-	dic1[ip]=arr
-	print str(dic1).replace('\'',"\"")
-else:
-	print 'All ports are closed'
+import collections
+p=raw_input()
+q = commands.getoutput('nmap %s' %p)
+# q = commands.getoutput('nmap 10.1.39.79-100')
+q=q.split('Nmap')
+# q=q.replace('\n','$').split('$$')
+dic1={}
+for i in range(2,len(q)-1):
+	s=q[i]
+	if 'PORT' in s:
+		s=s.split('\n')
+		s=filter(None,s)
+		ip = s[0].split(' ')[4]
+		arr = []
+		for j in range(4,len(s)):
+			data=filter(None,s[j].split(' '))
+ 			dic = {}
+ 			dic["PORT"]=str(data[0])
+ 			dic["STATE"]=data[1]
+ 			dic["SERVICE"] = data[2]
+ 			arr.append(dic)
+ 		dic1[ip]=arr
+	else:
+		s=s.split('\n')
+		s=filter(None,s)
+		ip = s[0].split(' ')[4]
+		dic1[ip]="All ports are close"
+print str(dic1).replace('\'',"\"")
